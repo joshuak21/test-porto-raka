fb7302e24f13ecee6f99a134f8f9b776ded4a6b6
joshuak21 <kurniawan.joshua21@gmail.com>
2024-01-06 23:34:00 +0800

    Finishing landing banner section and header component

---
+++ src/components/header.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import NextImage from 'next/image'
+
+import Styles from './header-style.module.css'
+
+import SidebarBg from '../../public/landing/sidebar-bg.png'
+
+export default function HeaderComponent(): JSX.Element {
+	const [sidebar, setSidebar] = useState(false)
+
+	function toggleSidebar(): void {
+		setSidebar(!sidebar)
+	}
+
+	return (
+		<header className="fixed w-full p-[100px] bg-transparent top-0 z-[2] flex justify-end">
+			<div className="relative z-[1]">
+				<button type="button" onClick={toggleSidebar}>
+					<NextImage src="./landing/menu-icon.png" alt="" width={48} height={48} />
+				</button>
+			</div>
+			<div className={`${Styles['sidebar-modal']} ${sidebar ? Styles['show'] : ''}`}>
+				<div className={`relative w-full h-full flex justify-end`}>
+					<NextImage src={SidebarBg} alt="" className="w-full h-full object-fill absolute z-[1]" />
+					<div className="relative z-[2] p-[100px] flex flex-col items-end max-w-[90%]">
+						<button type="button" onClick={toggleSidebar}>
+							<NextImage src="./landing/close-icon.png" width={48} height={48} alt="" />
+						</button>
+						<button type="button" className={`text-[#505050] text-[4em] flex flex-row ${Styles['header-content']}`}>
+							<i className="uil uil-arrow-down-right leading-[1] w-0 overflow-hidden opacity-0" />
+							<p className="leading-[1] w-auto text-left">PORTFOLIO</p>
+							<p className={Styles['header-content-subtitle']}>Selected works of<br />mine</p>
+						</button>
+						<div className="w-[80px] h-[2px] bg-[#505050] my-[1.25em] mr-[64px]" />
+						<button type="button" className={`text-[#505050] text-[4em] flex flex-row ${Styles['header-content']}`}>
+							<i className="uil uil-arrow-down-right leading-[1] w-0 overflow-hidden opacity-0" />
+							<p className="leading-[1] w-auto text-left">PERSONAL INFO</p>
+							<p className={Styles['header-content-subtitle']}>Get to know me<br />better</p>
+						</button>
+						<button type="button" className={`text-[#505050] text-[4em] flex flex-row ${Styles['header-content']}`}>
+							<i className="uil uil-arrow-down-right leading-[1] w-0 overflow-hidden opacity-0" />
+							<p className="leading-[1] w-auto text-left">EXPERIENCE</p>
+							<p className={Styles['header-content-subtitle']}>Working experiences<br />&amp; others</p>
+						</button>
+						<button type="button" className={`text-[#505050] text-[4em] flex flex-row ${Styles['header-content']}`}>
+							<i className="uil uil-arrow-down-right leading-[1] w-0 overflow-hidden opacity-0" />
+							<p className="leading-[1] w-auto text-left">CONTACTS</p>
+							<p className={Styles['header-content-subtitle']}>Get in touch<br />with me</p>
+						</button>
+						<div className="w-[80px] h-[2px] bg-[#505050] my-[1.25em] mr-[64px]" />
+					</div>
+				</div>
+			</div>
+		</header>
+	)
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import NextImage from 'next/image'
 
 import Styles from './style.module.css'
+import HeaderComponent from '@/components/header'
 
 export default function HomePage(): JSX.Element {
 	const [state, setState] = useState({
@@ -12,15 +13,12 @@ export default function HomePage(): JSX.Element {
 		setState({ isClicked: true })
 	}
 
-	return (
-		<>
+	return (<>
+		<HeaderComponent />
+		<main>
 			<section id="banner" className={`w-full h-full min-h-[100vh] relative bg-[#000000] ${Styles['banner-section']}`}>
 				<div className="flex flex-row justify-between items-center relative z-[1] p-[100px]">
 					<p className="text-white text-[1.5em]">Greetings!</p>
-					{/* <p className="text-white">Menu</p> */}
-					<button type="button">
-						<NextImage src="./landing/menu-icon.png" alt="" width={48} height={48} />
-					</button>
 				</div>
 				<div className={`absolute top-0 left-0 w-full h-full flex flex-row items-center justify-center ${Styles['banner-content']}`}>
 					<button onClick={toggleStartButton} type="button" className={`${Styles['button']} ${state.isClicked ? Styles['clicked'] : ''}`}>
@@ -30,9 +28,11 @@ export default function HomePage(): JSX.Element {
 					</button>
 				</div>
 			</section>
-			<section id="porto-section" className="w-full h-full min-h-[100vh] bg-[#000000]">
-				<p>Portfolio Sections</p>
-			</section>
-		</>
-	);
+			{state.isClicked && (<>
+				<section id="porto-section" className="w-full h-full min-h-[100vh] bg-[#000000]">
+					<p className="text-white text-[5em] font-bold">Portfolio Sections</p>
+				</section>
+			</>)}
+		</main>
+	</>)
 }
